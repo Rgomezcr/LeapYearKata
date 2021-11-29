3b9222c97677c5175dc2a99de910364bfafea2ac
Raúl Gómez <rgomez@voxelgroup.net>
2021-11-29 13:34:43 +0100

    Initial commit

---
+++ LeapYearKata.Tests/LeapYearShould.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace LeapYearKata.Tests
+{
+    public class LeapYearShould
+    {
+        [Fact]
+        public void CheckIfItsLeapYear()
+        {
+        }
+    }
+}
